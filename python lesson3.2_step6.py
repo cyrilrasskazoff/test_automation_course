821d5fe605990cff521eb6d0a10d43bafebbc9c5
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2022-12-09 18:23:13 +0300

    commit from module 3: assertion errors

--- python lesson3.2_step6.py
+++ python lesson3.2_step6.py
@@ -7,3 +7,12 @@ assert abs(-1) == 1
 # Для добавления дополнительного сообщения можно при вызове assert через запятую написать нужное сообщение, которое будет выведено в случае ошибки проверки результата:
 
 assert abs(-1) == -1, "Should be absolute value of a number"
+
+# сообщения об ошибках должны быть информативными, иногда имеет смысл пользоваться форматированием строк, например f-строками
+# def test_input_text(expected_result, actual_result):
+#     assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+
+# Иногда при работе с текстами не нужны жёсткие проверки на полное совпадение, и требуется проверить,
+# что некий текст является подстрокой другого текста. Это можно сделать с помощью ключевого слова in
+# def test_substring(full_string, substring):
+#     assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
