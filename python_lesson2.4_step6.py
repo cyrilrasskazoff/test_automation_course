2cdb51f2c85be79e9a55955b020f5dfd7bf2cc38
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2022-12-16 12:24:37 +0300

    экслицитное ожидание

--- python_lesson2.4_step6.py
+++ python_lesson2.4_step6.py
@@ -45,7 +45,7 @@ import math
 try:
     browser = webdriver.Chrome()
     browser.get("http://suninjuly.github.io/wait2.html")
-    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    # говорим Selenium ждать в течение 5 секунд пока кнопка станет активной
     button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "verify"))) # element_to_be_clickable вернет элемент, когда он станет кликабельным, или вернет False
     button.click()
     message = browser.find_element(By.ID, "verify_message")
