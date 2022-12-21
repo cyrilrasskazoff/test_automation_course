f0f30fadeaff3077e2ddbf658aa14c7bbb91705f
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2022-12-21 18:12:37 +0300

    Автоиспользование фикстур

--- test_fixture5.py
+++ test_fixture5.py
@@ -10,7 +10,7 @@ url = "http://selenium1py.pythonanywhere.com/"
 
 
 @pytest.fixture
-def driver():
+def browser():
     print("\nstart browser for test..")
     driver = webdriver.Chrome()
     yield driver
@@ -23,15 +23,15 @@ def prepare_data():
     print("preparing some critical data for every test")
 
 
-class TestMainPage1():
-    def test_guest_should_see_login_link(self, driver):
+class TestMainPage1:
+    def test_guest_should_see_login_link(self, browser):
         # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
-        driver.get(url)
-        driver.find_element(By.CSS_SELECTOR, "#login_link")
+        browser.get(url)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    def test_guest_should_see_basket_link_on_the_main_page(self, driver):
-        driver.get(url)
-        driver.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(url)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
 
 # для каждого теста фикстура подготовки данных выполнилась без явного вызова.
 # Нужно быть аккуратнее с этим параметром, потому что фикстура выполняется для всех тестов.
