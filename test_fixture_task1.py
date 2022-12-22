43ebf1a2388873970cf3e335ce3f6b260014368b
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2022-12-22 16:51:35 +0300

    параметры xfail

---
+++ test_fixture_task1.py
@@ -0,0 +1,19 @@
+# параметры xfail
+# параметр, который в случае неожиданного прохождения теста, помеченного как xfail, отметит в отчете этот тест как упавший - (strict)
+# https://docs.pytest.org/en/latest/reference/reference.html#pytest.mark.xfail
+import pytest
+
+
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
