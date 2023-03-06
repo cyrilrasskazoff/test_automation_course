846be56830c34718bece6b071a98391d6eb87e2b
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-03-06 17:30:25 +0300

    Переключение на новую вкладку

--- python_lesson2.3_step6.py
+++ python_lesson2.3_step6.py
@@ -12,15 +12,19 @@ from selenium.webdriver.common.by import By
 import time
 import math
 
+
 def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
 try:
     url = "http://suninjuly.github.io/redirect_accept.html"
     browser = webdriver.Chrome()
     browser.get(url)
 
     button_1 = browser.find_element(By.XPATH, '//button')
-    browser.execute_script("document.getElementsByTagName('button')[0].classList.remove('trollface');") # это скрипт для удаления класса, с помощью которого кнопка перемещается
+    browser.execute_script(
+        "document.getElementsByTagName('button')[0].classList.remove('trollface');")  # это скрипт для удаления класса, с помощью которого кнопка перемещается
     button_1.click()
 
     new_window = browser.window_handles[1]
@@ -37,8 +41,6 @@ try:
     button_2.click()
 
     time.sleep(1)
-
-
 
 finally:
     time.sleep(10)
