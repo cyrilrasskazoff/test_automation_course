63a4c6f6e94337e4db602bbb7e20944b471f8cf5
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-10-16 14:56:46 +0300

    file upload

--- python_lesson2.2_step8.py
+++ python_lesson2.2_step8.py
@@ -3,8 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-#file_path = 'D:\\python_work\\Automation_Testing_Sel\\text_file.txt'
-file_path = '/Users/kirillrasskazov/Documents/AutomationSelenium/text_file.txt'
+# простой способ: прямо объявить путь к файлу. недостаток - придется переписывать каждый раз для нового окружения
+# (например, другая ОС, другой компьютер)
+# file_path = 'D:\\python_work\\Automation_Testing_Sel\\text_file.txt'
+# file_path = '/Users/kirillrasskazov/Documents/AutomationSelenium/text_file.txt'
+
+#  ****************
+# оптимальный способ: Чтобы указать путь к файлу, можно использовать стандартный модуль Python для работы с
+# операционной системой — os. В этом случае ваш код не будет зависеть от операционной системы, которую вы используете.
+import os
+current_dir = os.path.abspath(os.path.dirname(__file__))  # получаем путь к директории текущего исполняемого файла
+file_path = os.path.join(current_dir, 'text_file.txt')    # добавляем к этому пути имя файла
+
 try:
     url = "http://suninjuly.github.io/file_input.html"
     browser = webdriver.Chrome()
@@ -26,9 +36,8 @@ try:
     time.sleep(1)
 
 
-
 finally:
-    time.sleep(10)
+    time.sleep(5)
     browser.quit()
 
 # не забываем оставить пустую строку в конце файла!
