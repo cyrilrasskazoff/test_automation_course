6ac28784777ff34d844b642ee25dff8ca3891d42
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-10-16 15:24:05 +0300

    alerts - работа с модальными окнами

--- python_lesson2.3_step4.py
+++ python_lesson2.3_step4.py
@@ -3,12 +3,16 @@
 # alert = browser.switch_to.alert - (переключение на модальное окно)
 # alert.accept() - принимаем Alert (= нажать на OK)
 
-# если нужно получить текст из alert, используйте свойство text объекта alert: 1. alert = browser.switch_to.alert 2. alert_text = alert.text
+# если нужно получить текст из alert, используйте свойство text объекта alert:
+# 1. alert = browser.switch_to.alert
+# 2. alert_text = alert.text
+
 
 # 2 тип Confirm: модальное окно с кнопками 'OK' и 'Cancel' - предлагает либо согласиться либо отказаться от сообщения:
 # confirm = browser.switch_to.alert
 # confirm.accept() - соглашаемся
 # confirm.dismiss() - не соглашаемся
+
 
 # 3 тип Prompt - имеет дополнительное поле для ввода текста. Чтобы ввести текст, используйте метод send_keys()
 # prompt = browser.switch_to.alert
@@ -20,8 +24,11 @@ from selenium.webdriver.common.by import By
 import time
 import math
 
+
 def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
 try:
     url = "http://suninjuly.github.io/alert_accept.html"
     browser = webdriver.Chrome()
@@ -43,7 +50,6 @@ try:
     button_2.click()
 
     time.sleep(1)
-
 
 
 finally:
