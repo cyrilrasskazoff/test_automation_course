2d3630c3f2991458ebe20b4da85c6307fe893e40
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-10-24 11:24:25 +0300

    dropdown list

--- python_lesson2.2_step3.py
+++ python_lesson2.2_step3.py
@@ -2,7 +2,8 @@
 # browser.find_element(By.{choose a necessary selector}, "{}").click() - находим выпадающий список и кликаем по нему
 # browser.find_element(By.{choose a necessary selector}, "{}").click() - находим нужный вариант в списке и кликаем по нему
 
-# класс Select - позволяет избегать метод click():
+# **************
+# класс Select - позволяет избегать повторное использоване метода click():
 # from selenium.webdriver.support.ui import Select
 # dropdown = Select(browser.find_element(By.TAG_NAME, "select")) - инициализируем новый объект списка, передав в него WebElement с тегом select
 # dropdown.select_by_value("value") OR dropdown.select_by_visible_text("text") OR dropdown.select_by_index(index) - находим любой вариант из списка
