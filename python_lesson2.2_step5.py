094d6c85e79d2e82599b7331b1e717f6fbf7f128
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-10-24 11:37:36 +0300

    scripts

--- python_lesson2.2_step5.py
+++ python_lesson2.2_step5.py
@@ -4,21 +4,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-browser = webdriver.Chrome()
-link = "https://SunInJuly.github.io/execute_script.html"
-browser.get(link)
-# приведенный ниже код вернет ошибку, т.к. кнопка перекрыта другим элементом страницы
-# button = browser.find_element(By.TAG_NAME, "button")
-# time.sleep(5)
-# button.click()
-# browser.quit()
+try:
+    browser = webdriver.Chrome()
+    link = "https://SunInJuly.github.io/execute_script.html"
+    browser.get(link)
+    # приведенный ниже код вернет ошибку, т.к. кнопка перекрыта другим элементом страницы
+    # button = browser.find_element(By.TAG_NAME, "button")
+    # time.sleep(5)
+    # button.click()
+    # browser.quit()
 
-# используем скрипт для прокрутки страницы
-button = browser.find_element(By.TAG_NAME, "button")
-time.sleep(5)
-browser.execute_script("return arguments[0].scrollIntoView(true);", button)
-time.sleep(5)
-button.click()
-browser.quit()
+    # используем скрипт для прокрутки страницы
+    button = browser.find_element(By.TAG_NAME, "button")
+    time.sleep(5)
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    time.sleep(5)
+    button.click()
 
-# не забываем оставить пустую строку в конце файла!
+finally:
+    browser.quit()
+
+    # не забываем оставить пустую строку в конце файла!
