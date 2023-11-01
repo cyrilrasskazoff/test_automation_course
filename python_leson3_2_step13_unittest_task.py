ae758564545afcf559de9fa8eb685de97ea690f2
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-11-01 19:50:48 +0300

    unittest practice

--- python_leson3_2_step13_unittest_task.py
+++ python_leson3_2_step13_unittest_task.py
@@ -16,50 +16,46 @@ class TestWelcomeText(unittest.TestCase):
         self.driver.quit()
 
     def test_search_element_1(self):
-        driver = webdriver.Chrome()
-        driver.get(url)
+        self.driver.get(url)
 
-        input1 = driver.find_element(By.XPATH, '//form/div[1]/div[1]/input')
+        input1 = self.driver.find_element(By.XPATH, '//form/div[1]/div[1]/input')
         input1.send_keys("FirstName")
-        input2 = driver.find_element(By.XPATH, '//form/div[1]/div[2]/input')
+        input2 = self.driver.find_element(By.XPATH, '//form/div[1]/div[2]/input')
         input2.send_keys("LasttName")
-        input3 = driver.find_element(By.XPATH, '//form/div[1]/div[3]/input')
+        input3 = self.driver.find_element(By.XPATH, '//form/div[1]/div[3]/input')
         input3.send_keys("test@te.st")
 
-        button = driver.find_element(By.CSS_SELECTOR, "button.btn")
+        button = self.driver.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
 
         time.sleep(1)
 
         # находим элемент, содержащий текст
-        welcome_text = driver.find_element(By.TAG_NAME, "h1")
+        welcome_text = self.driver.find_element(By.TAG_NAME, "h1")
         text = welcome_text.text
         self.assertEqual(text, "Congratulations! You have successfully registered!", "Welcome text error!")
         time.sleep(5)
-        driver.quit()
 
     def test_search_element_2(self):
-        driver = webdriver.Chrome()
-        driver.get(url_1)
+        self.driver.get(url_1)
 
-        input1 = driver.find_element(By.XPATH, '//form/div[1]/div[1]/input')
+        input1 = self.driver.find_element(By.XPATH, '//form/div[1]/div[1]/input')
         input1.send_keys("FirstName")
-        input2 = driver.find_element(By.XPATH, '//form/div[1]/div[2]/input')
+        input2 = self.driver.find_element(By.XPATH, '//form/div[1]/div[2]/input')
         input2.send_keys("LasttName")
-        input3 = driver.find_element(By.XPATH, '//form/div[1]/div[3]/input')
+        input3 = self.driver.find_element(By.XPATH, '//form/div[1]/div[3]/input')
         input3.send_keys("test@te.st")
 
-        button = driver.find_element(By.CSS_SELECTOR, "button.btn")
+        button = self.driver.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
 
         time.sleep(1)
 
         # находим элемент, содержащий текст
-        welcome_text = driver.find_element(By.TAG_NAME, "h1")
+        welcome_text = self.driver.find_element(By.TAG_NAME, "h1")
         text = welcome_text.text
         self.assertEqual(text, "Congratulations! You have successfully registered!", "Welcome text error!")
         time.sleep(5)
-        driver.quit()
 
 
 if __name__ == "__main__":
