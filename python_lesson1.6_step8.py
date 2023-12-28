13fbc7390b4a1bf3a175a55115a9a8ecc9d23fc5
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2023-12-28 15:24:38 +0300

    Поиск по XPath

--- python_lesson1.6_step8.py
+++ python_lesson1.6_step8.py
@@ -23,7 +23,8 @@ try:
     input3.send_keys("Smolensk")
     input4 = browser.find_element(By.ID, 'country')
     input4.send_keys("Russia")
-    button = browser.find_element(By.XPATH, '//button[text()="Submit"]')
+    button = browser.find_element(By.XPATH, '//button[text()="Submit"]') #<button type="submit" class="btn"
+    # disabled="disabled">Submit</button>
     button.click()
 
 finally:
